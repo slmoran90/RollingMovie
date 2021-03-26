8ae9235ea2659954c3f034a826056205765fc51e
MCordich <martincordich@gmail.com>
2021-03-26 14:59:55 -0300

    Se termino la base de la pagina Admin

---
+++ js/peliClass.js
@@ -0,0 +1,9 @@
+export class Pelicula {
+    constructor(codigo, nombre, categoria, descripcion, publicado) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.descripcion = descripcion;
+        this.publicado = publicado;
+    }
+}
